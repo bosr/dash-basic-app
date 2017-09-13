6693f9151e607abd2eb638b9699ef32ace4231c9
Romain Bossart <romain.bossart@fastmail.com>
2017-09-13 18:23:47 +0200

    2. basic text input and graph

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,23 @@
 import dash
+import dash_html_components as html
+import dash_core_components as dcc
 
 my_app = dash.Dash('my app')
 
-from dash_html_components import H1, Textarea
-from dash_core_components import Graph
-
-my_app.layout = H1('Hello PlotCon')
+my_app.layout = html.Div([
+    html.H1('Hello PlotCon'),
+    html.Label('Stock Tickers'),
+    dcc.Input(
+        value='TLSA',
+        id='my-input'),
+    dcc.Graph(
+        id='my-graph',
+        figure={
+            'data': [
+                {'x': [1, 2], 'y': [3, 1]}
+            ]
+        }
+    )
+])
 
 my_app.server.run(debug=True)
